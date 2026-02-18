f0bb276e1a6010941361c8fd9038ea7c70a5f3d8
Mueed <69814274+TheAM01@users.noreply.github.com>
2026-02-18 05:35:46 +0500

    added linkedin

--- routes.config.js
+++ routes.config.js
@@ -16,6 +16,7 @@ export default {
         new Link('spotify', 'open.spotify.com/artist/2CTBnkHNgYSjaXtnHiIXAA?si=iC81rf7GSU6xbZQ31212qA'),
         new Link('solid-pixel', '194.164.125.5:6024/'),
         new Link('pfh', 'parhle.glitch.me'),
-        new Link("unreleased", "drive.google.com/drive/u/4/folders/16BfK1RS1Ybqcy22eweRqe8VSIoE5Wso0")
+        new Link("unreleased", "drive.google.com/drive/u/4/folders/16BfK1RS1Ybqcy22eweRqe8VSIoE5Wso0"),
+        new Link("linkedin", "www.linkedin.com/in/mueeeeed")
     ]
 }
